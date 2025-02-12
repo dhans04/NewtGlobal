18a47f92877c646dc0b57671d47379f7c6226bb3
dhans04 <136153441+dhans04@users.noreply.github.com>
2025-02-12 19:48:51 +0530

    Add files via upload

---
+++ FullStack/Inheritance/Fish/Fish.java
@@ -0,0 +1,20 @@
+package org.studyeasy.Fish;
+import org.studyeasy.Animal.Animal;
+public class Fish extends Animal{
+
+        protected boolean waterBone;
+        protected boolean gills;
+
+        public Fish() {
+            animalType = "fish";
+            this.waterBone = true;
+            this.gills = true;
+        }
+
+        public String showInfo() {
+            return "Fish [waterBone=" + waterBone + ", gills=" + gills + ", heightInFeet=" + heightInFeet
+                    + ", weightInKilos=" + weightInKilos + ", animalType=" + animalType + ", bloodType=" + bloodType + "]";
+        }
+
+
+}
