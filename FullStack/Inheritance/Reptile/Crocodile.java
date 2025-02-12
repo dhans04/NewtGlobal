1188aec6fcae513d3630c72f0b85dc2eb9ad6c24
dhans04 <136153441+dhans04@users.noreply.github.com>
2025-02-12 19:47:33 +0530

    Add files via upload

---
+++ FullStack/Inheritance/Reptile/Crocodile.java
@@ -0,0 +1,16 @@
+package org.studyeasy.Reptile;
+
+public class Crocodile extends Reptile{
+
+        public Crocodile() {
+            super();
+            egg = "Hard Shelled";
+        }
+
+        public String showInfo() {
+            return "Crocodile [skin=" + skin + ", egg=" + egg + ", backbone=" + backbone + ", heightInFeet=" + heightInFeet
+                    + ", weightInKilos=" + weightInKilos + ", animalType=" + animalType + ", bloodType=" + bloodType + "]";
+        }
+
+
+}
