443deb85adcc4bece358d6242ae99a384154c505
dhans04 <136153441+dhans04@users.noreply.github.com>
2025-02-12 19:44:44 +0530

    Add files via upload

---
+++ FullStack/Inheritance/Birds/Birds.java
@@ -0,0 +1,14 @@
+package org.studyeasy.Birds;
+import org.studyeasy.Animal.*;
+public class Birds extends Animal{
+        protected boolean feather;
+        protected boolean canFly;
+
+        public Birds() {
+            this.feather = true;
+            this.canFly = true;
+        }
+
+
+}
+
---
+++ FullStack/Inheritance/Birds/Eagle.java
@@ -0,0 +1,11 @@
+package org.studyeasy.Birds;;
+public class Eagle extends Birds {
+
+
+        public String showInfo() {
+            return "Eagle [feather=" + feather + ", canFly=" + canFly + ", heightInFeet=" + heightInFeet
+                    + ", weightInKilos=" + weightInKilos + ", animalType=" + animalType + ", bloodType=" + bloodType + "]";
+        }
+
+    }
+
